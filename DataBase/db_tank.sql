d6041bb45ea65e5f7709a83e088f3661ea9b6097
Felix Wee <weifengan@163.com>
2018-01-15 23:37:33 +0800

    数据库account表中添加等级字段

--- DataBase/db_tank.sql
+++ DataBase/db_tank.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50716
 File Encoding         : 65001
 
-Date: 2018-01-10 22:33:14
+Date: 2018-01-15 21:11:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,6 +25,7 @@ CREATE TABLE `account` (
   `acc_pwd` varchar(30) NOT NULL,
   `acc_role` int(11) DEFAULT '-1',
   `acc_nick` varchar(30) DEFAULT '',
+  `acc_level` int(11) DEFAULT '1',
   `acc_coin` int(11) DEFAULT '500',
   `acc_diamond` int(11) DEFAULT '0',
   `acc_email` varchar(255) DEFAULT '',
@@ -37,8 +38,8 @@ CREATE TABLE `account` (
 -- ----------------------------
 -- Records of account
 -- ----------------------------
-INSERT INTO `account` VALUES ('1', 'frank', '123', '1', '周测试', '500', '0', '', '', '2018-01-10 19:36:26', '2018-01-10 21:19:33');
-INSERT INTO `account` VALUES ('2', 'abc', '1234', '-1', '', '500', '0', '', '', '2018-01-10 19:36:31', '2018-01-10 19:36:31');
-INSERT INTO `account` VALUES ('3', 'frank1', '1232', '-1', '', '500', '0', '', '', '2018-01-10 19:35:41', '2018-01-10 19:35:41');
-INSERT INTO `account` VALUES ('4', 'aaa', 'bbb', '-1', '', '500', '0', '', '', '2018-01-10 19:35:42', '2018-01-10 19:35:42');
-INSERT INTO `account` VALUES ('5', 'bbbb', 'aaaad', '-1', '', '500', '0', '', '', '2018-01-10 19:35:43', '2018-01-10 19:35:43');
+INSERT INTO `account` VALUES ('1', 'frank', '123', '1', '周测试', '1', '500', '0', '', '', '2018-01-10 19:36:26', '2018-01-10 21:19:33');
+INSERT INTO `account` VALUES ('2', 'abc', '123', '3', 'test', '1', '500', '0', '', '', '2018-01-10 19:36:31', '2018-01-13 23:04:48');
+INSERT INTO `account` VALUES ('3', 'frank1', '1232', '-1', '', '1', '500', '0', '', '', '2018-01-10 19:35:41', '2018-01-10 19:35:41');
+INSERT INTO `account` VALUES ('4', 'aaa', 'bbb', '-1', '', '1', '500', '0', '', '', '2018-01-10 19:35:42', '2018-01-10 19:35:42');
+INSERT INTO `account` VALUES ('5', 'bbbb', 'aaaad', '-1', '', '1', '500', '0', '', '', '2018-01-10 19:35:43', '2018-01-10 19:35:43');
